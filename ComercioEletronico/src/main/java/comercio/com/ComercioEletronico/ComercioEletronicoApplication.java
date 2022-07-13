7049a367a560ba0499666be16046f5a92eb5dd05
Davy Lucas <61299543+KURAMA2012@users.noreply.github.com>
2022-07-13 17:38:38 -0300

    Criando Projeto Spring

---
+++ ComercioEletronico/src/main/java/comercio/com/ComercioEletronico/ComercioEletronicoApplication.java
@@ -0,0 +1,13 @@
+package comercio.com.ComercioEletronico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComercioEletronicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComercioEletronicoApplication.class, args);
+	}
+
+}
---
+++ ComercioEletronico/src/test/java/comercio/com/ComercioEletronico/ComercioEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package comercio.com.ComercioEletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
